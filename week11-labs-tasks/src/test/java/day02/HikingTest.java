0e6791720e5caab9aaa4e18e318d58e91240faf7
k-tam-8 <fluff8@gmail.com>
2022-01-05 18:18:14 +0100

    day02

--- week11-labs-tasks/src/test/java/day02/HikingTest.java
+++ week11-labs-tasks/src/test/java/day02/HikingTest.java
@@ -14,5 +14,8 @@ class HikingTest {
         List<Double> heightList = Arrays.asList(10.0,20.0,15.0,18.0);
         Hiking hiking = new Hiking(heightList);
         assertEquals(13.0,hiking.getPlusElevation(hiking.getHeightList()));
+        List<Double> heightList2 = Arrays.asList(10.0,20.0, 25.0,15.0,18.0);
+        Hiking hiking2 = new Hiking(heightList2);
+        assertEquals(18.0,hiking2.getPlusElevation(hiking2.getHeightList()));
     }
 }
